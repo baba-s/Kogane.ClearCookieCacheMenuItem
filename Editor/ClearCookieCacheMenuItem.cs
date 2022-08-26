7fbe8ed620e8ec6a6ac562be6a423e080cb91d10
baba-s <shotababa1206@gmail.com>
2022-08-26 21:47:14 +0900

    add

---
+++ Editor/ClearCookieCacheMenuItem.cs
@@ -0,0 +1,31 @@
+﻿using UnityEditor;
+using UnityEngine.Networking;
+
+namespace Kogane.Internal
+{
+    internal static class ClearCookieCacheMenuItem
+    {
+        [MenuItem( "Kogane/UnityWebRequest/Clear Cookie Cache" )]
+        private static void ClearCookieCache()
+        {
+            var isOk = EditorUtility.DisplayDialog
+            (
+                title: "",
+                message: "UnityWebRequest の ClearCookieCache を実行しますか？",
+                ok: "はい",
+                cancel: "いいえ"
+            );
+
+            if ( !isOk ) return;
+
+            UnityWebRequest.ClearCookieCache();
+
+            EditorUtility.DisplayDialog
+            (
+                title: "",
+                message: "UnityWebRequest の ClearCookieCache を実行しました",
+                ok: "OK"
+            );
+        }
+    }
+}
